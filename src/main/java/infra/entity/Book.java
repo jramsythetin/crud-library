6dc5b5399561a397813f4dd6c987ed42bdeae108
Jram <jramsythetin@gmail.com>
2025-04-17 21:40:25 -0300

    first commit

---
+++ src/main/java/infra/entity/Book.java
@@ -0,0 +1,27 @@
+package infra.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table (name = "book")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name", length = 250)
+    String name;
+    @Column (name = "author", length = 250)
+    String author;
+    @Column (name = "genre", length = 40)
+    String genre;
+    @Column (name = "isbn", length = 14)
+    String isbn;
+}
