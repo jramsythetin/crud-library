5626e95e6bd0717982ba966b008a189cc20436be
Jram <jramsythetin@gmail.com>
2025-04-29 13:10:54 -0300

    atualização

--- src/main/java/library/infra/entity/Book.java
+++ src/main/java/library/infra/entity/Book.java
@@ -5,6 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Setter
@@ -12,7 +17,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Entity
 @Table (name = "book")
-public class Book {
+public class Book implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -24,4 +29,19 @@ public class Book {
     private String genre;
     @Column (name = "isbn", length = 14)
     private String isbn;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "password";
+    }
+
+    @Override
+    public String getUsername() {
+        return "email";
+    }
 }
